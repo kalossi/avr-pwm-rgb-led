2371651101893f5c33e24458615b33155065a34d
kalossi <kalossi@gmail.com>
2026-01-06 12:41:51 +0200

    better c standard habbit and conversion logic

--- main.c
+++ main.c
@@ -5,7 +5,8 @@
 void ADC_init(void)
 {
     // AVcc reference
-    ADMUX = (1 << REFS0) | (0 << ADLAR);
+    ADMUX = (1 << REFS0);
+    ADMUX& ~(1 << ADLAR);
 
     // Enable ADC, prescaler = 64 (8MHz / 64 = 125kHz)
     ADCSRA = (1 << ADEN) |
@@ -14,13 +15,20 @@ void ADC_init(void)
 
 uint8_t ADC_read(uint8_t channel)
 {
-    // Select channel (0–7)
+    // Select channel (0–7) by masking top and bottom bits
+    // ADMUX bits 3:0 are a channel select and the binary representation of the channel correlates ot DECIMAL values 0-7
     ADMUX = (ADMUX & 0xF0) | (channel & 0x0F);
 
     // Start conversion
     ADCSRA |= (1 << ADSC);
 
     // Wait for conversion to finish
+    while (ADCSRA & (1 << ADSC));
+
+    // At 10-bit resolution with 125 kHz ADC clock (with prescaler 128), one conversion takes ~7-8 µs × 13 cycles = ~101-102 µs
+
+    // Only trust the second conversion result
+    ADCSRA |= (1 << ADSC);
     while (ADCSRA & (1 << ADSC));
 
     // Return 8 MSBs
