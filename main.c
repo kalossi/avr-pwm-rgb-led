d8a8581b77ea5b73b5cf504a91713efc3cd49ac7
kalossi <kalossi@gmail.com>
2026-01-12 11:46:32 +0200

    experimenting with interrupts

--- main.c
+++ main.c
@@ -1,20 +1,27 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
+#define ADCPIN 0 // ADC0
+
 int main(void)
 {
     DDRB = 0xFF;
+    DDRC = 0x00;
+    // DIDR0 = (1 << ADC0D);                               // disable digital buffer
+    ADMUX = (1 << REFS0);                               // AVcc reference
+    ADCSRA = (1 << ADEN) | (1 << ADIE) | (1 << ADPS2) | (1 << ADPS1); // prescaler 64 = (8MHz / 64 = 125KHz)
+    sei();
 
-    ADMUX = (1 << REFS0);                  // AVcc ref
-    ADCSRA = (1 << ADEN) |                 // ADC enable
-             (1 << ADPS2) | (1 << ADPS1);  // prescaler 64
 
     while (1)
     {
-        ADCSRA |= (1 << ADSC);
-        while (ADCSRA & (1 << ADSC))
-            ;
-
-        PORTB = ~ADCL;
+        ADMUX |= (ADMUX & 0xF0) | (ADCPIN & 0x0F); // select ADC channel and mask the rest ADMUX
+        ADCSRA |= (1 << ADSC);         // start conversion
+        while (ADCSRA & (1 << ADSC));  // wait
     }
 }
+
+ISR(ADC_vect)
+{
+    PORTB = ~ADCL; // read ADC low byte and output to PORTB
+}   
