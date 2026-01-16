0147c9d662f7e31249308a28299c9981a122bfb2
kalossi <kalossi@gmail.com>
2026-01-16 19:48:48 +0200

    minor alterations to PWM control registers and more commenting

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <avr/interrupt.h>
 #include <avr/sleep.h>
 
+/* ADC and PWM initialization */
 void ADC_init(void)
 {
     ADMUX  = (1 << REFS0);                 // AVcc reference, start on ADC0
@@ -14,16 +15,17 @@ void ADC_init(void)
 
 void PWM_init(void)
 {
-    TCCR0A = (1 << COM0A1) | (1 << WGM01) | (1 << WGM00);
-    TCCR0B = (1 << CS01);
+    TCCR0A = (1 << COM0A1) | (1 << WGM01) | (1 << WGM00); // Fast PWM
+    TCCR0B = (1 << CS01); // Prescaler 8
 
-    TCCR1A = (1 << COM1A1) | (1 << WGM11);
-    TCCR1B = (1 << WGM13) | (1 << WGM12) | (1 << CS11);
+    TCCR1A = (1 << COM1A1) | (1 << WGM12) | (1 << WGM10); // Fast PWM 8-bit
+    TCCR1B = (1 << WGM13) | (1 << WGM12) | (1 << CS11); // Prescaler 8
 
-    TCCR2A = (1 << COM2A1) | (1 << WGM21) | (1 << WGM20);
-    TCCR2B = (1 << CS22);
+    TCCR2A = (1 << COM2A1) | (1 << WGM21) | (1 << WGM20); // Fast PWM
+    TCCR2B = (1 << CS21); // Prescaler 8
 }
 
+/* ADC interrupt service routine - fires when ADC conversion is complete */
 ISR(ADC_vect)
 {
     static uint8_t ch = 0;
@@ -35,7 +37,13 @@ ISR(ADC_vect)
         case 2: OCR2A = v >> 2; break;
     }
 
-    ch = (ch + 1) % 3;
+    // if is more faster for AVR to do increment and compare than modulo
+    ch++;
+    if (ch >= 3)
+    {
+        ch = 0;
+    }
+
     ADMUX = (ADMUX & 0xF0) | ch;            // Select next channel
     ADCSRA |= (1 << ADSC);                  // Start next conversion
 }
@@ -48,9 +56,12 @@ int main(void)
     ADC_init();
     PWM_init();
 
+    // Enable sleep mode - SLEEP_MODE_IDLE means CPU is stopped, peripherals work
     set_sleep_mode(SLEEP_MODE_IDLE);
+    // After every initialization enable global interrupts
     sei();
 
+    // Main loop. CPU sleeps, ADC and PWM work in background
     while (1) {
         sleep_cpu();
     }
